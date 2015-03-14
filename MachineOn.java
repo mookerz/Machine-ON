0f721c0cf36cab4b6cd165734d91f01b2a2c9b99
Guanlong Zhou <zhouguanlong2012@gmail.com>
2015-03-13 21:44:53 -0500

    Java Basic done

--- MachineOn.java
+++ MachineOn.java
@@ -5,6 +5,7 @@ import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.net.URLEncoder;
 
 
 class MachineOn
@@ -12,6 +13,7 @@ class MachineOn
 	public static void main(String[] args){
 		String Version = "1.0.0";
 		String SysOS = "Unkown", IPAddr = "Unkown", MacAddr = "Unkown", HostName = "Unkown";
+		String HTTPKey = "Error";
 		System.out.println();
 		System.out.println("**Starting MachineOn "+ Version + " by Mizzou1.com" );
 		System.out.println("**Author: Guanlong Zhou  @  www.WhoIsLong.com" );
@@ -34,13 +36,9 @@ class MachineOn
 		try {
 
 			ip = InetAddress.getLocalHost();
-			IPAddr = ip.getHostAddress();
 			NetworkInterface network = NetworkInterface.getByInetAddress(ip);
 			HostName = InetAddress.getLocalHost().getHostName();
 			byte[] mac = network.getHardwareAddress();
-
-
-
 			StringBuilder sb = new StringBuilder();
 			for (int i = 0; i < mac.length; i++) {
 				sb.append(String.format("%02X%s", mac[i], (i < mac.length - 1) ? "-" : ""));		
@@ -66,12 +64,97 @@ class MachineOn
 		String UserName = System.getProperty("user.name");
 		System.out.println();
 		System.out.println("Current MAC address : " + MacAddr);
-		System.out.println("Current IP address : " + IPAddr);
 		System.out.println("Host Name : " + HostName);
 		System.out.println("Machine OS : " + SysOS);
 		System.out.println("Machine local time : " + SysTime);
-		System.out.println("*UserName : " + UserName +" <- This is the key(IMPORTANT!)");
-		System.out.println("Machine local time : " + SysTime);
-		
+		System.out.println("Connecting to Server... Please wait...");
+
+		HTTPKey=HttpRequest.sendPost("http://localhost/MachineOnZend/api/GetKey.php", "key=123&v=456");
+		if(HTTPKey.toLowerCase().contains("error") ){
+			System.out.println("\n**Server Connection Error!");
+			System.out.println("**Please check your Internet connection and try again!");
+			System.out.println("Program exit.");
+			System.exit(0); 
+		}
+
+		IPAddr = HttpRequest.sendPost("http://ip.mizzou1.com", "");
+		IPAddr = PrefixIP(IPAddr);
+		//String CurrentResult = HttpRequest.sendPost("http://localhost/MachineOnZend/api/PostTest.php", "key=" + URLEncoder.encode(HTTPKey) + "&" + "value=" +URLEncoder.encode("123 456 \n www.sina.com&Me"));
+		//String CurrentResult = HttpRequest.sendPost("http://localhost/MachineOnZend/api/PostTest.php", "key=" + URLEncoder.encode(HTTPKey) + "&" + "value=123123");
+		//System.out.println("Input Test: "+ CurrentResult);
+
+		String CurrentResult = HttpRequest.sendPost("http://localhost/MachineOnZend/api/CreateStatus.php", "key=" + URLEncoder.encode(HTTPKey) + "&" + "OS=" + URLEncoder.encode(SysOS) + "&" + "MAC=" + URLEncoder.encode(" " + MacAddr + " ") + "&" + "IPAddr=" + URLEncoder.encode(IPAddr) + "&" + "UserName=" + URLEncoder.encode(UserName) );
+
+		if(CurrentResult.toLowerCase().contains("error")){
+			System.out.println("\n**Server Connection Error!");
+			System.out.println("**Please check your Internet connection and try again!");
+			System.out.println("Program exit.");
+			System.exit(0); 
+		}
+
+		String[] IDs = CurrentResult.split(" ", 2);
+		String MachineID = IDs[0];
+		String RecordID = IDs[1];
+
+		System.out.println("\n** Please write down the following information**");
+		System.out.println("Current IP: "+ IPAddr);
+		System.out.println("Current Key: "+ HTTPKey);
+		//System.out.println("Current Content Test: "+ CurrentResult);
+		System.out.println("Machine ID: "+ MachineID);
+		System.out.println("Record ID: "+ RecordID);
+		System.out.println("UserName : " + UserName +" <- This is the key (IMPORTANT!)");
+		System.out.println("Check status at on.mizzou1.com");
+		System.out.println();
+
+		try {
+			while (true) {
+				String UpdateResult = HttpRequest.sendPost("http://localhost/MachineOnZend/api/UpdateStatus.php", "key=" + URLEncoder.encode(HTTPKey) + "&" + "RecordID=" + URLEncoder.encode(RecordID) );
+				System.out.println(UpdateResult);
+				if(UpdateResult.toLowerCase().contains("error")){
+					System.out.println("\nServer Connection Error at " + new Date());
+					System.out.println("Program exit.");
+					System.exit(0); 
+				}
+
+				Thread.sleep(30 * 1000);
+			}
+		} catch (InterruptedException e) {
+			//e.printStackTrace();
+			System.out.println("\nServer Connection Error at " + new Date());
+			System.out.println("Program exit.");
+			System.exit(0); 
+		}
+
+
 	}
+
+
+
+
+
+
+
+
+	private static String PrefixIP(String input){
+		int IPLen = input.length();
+		String output = "";
+		for(int i =0; i<IPLen; i++){
+			if( (input.charAt(i)>'0' && input.charAt(i)<'9') || input.charAt(i)=='.' ){
+				output += input.charAt(i);
+			}
+		}
+		return output;
+	}
+
+	private static String PrefixNumber(String input){
+		int IPLen = input.length();
+		String output = "";
+		for(int i =0; i<IPLen; i++){
+			if( (input.charAt(i)>'0' && input.charAt(i)<'9')){
+				output += input.charAt(i);
+			}
+		}
+		return output;
+	}
+
 }
